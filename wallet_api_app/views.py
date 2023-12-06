0c39fc3e3fa242dc710f4100a3de1f109eca9076
mikegyamfi <codeage20@gmail.com>
2023-12-06 12:29:59 +0000

    mtn other

--- wallet_api_app/views.py
+++ wallet_api_app/views.py
@@ -144,7 +144,7 @@ def send_ishare_bundle(first_name: str, last_name: str, buyer, receiver: str, em
 
     token = bearer_token_collection.document("Active_API_BoldAssure")
     token_doc = token.get()
-    token_doc_dict = token_doc.dict()
+    token_doc_dict = token_doc.to_dict()
     tokennn = token_doc_dict['ishare_bearer']
 
     headers = {
