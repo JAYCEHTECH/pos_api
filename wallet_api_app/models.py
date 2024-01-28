bb67bc6153bcb717d8bbe242b747639c50e5dcc8
mikegyamfi <codeage20@gmail.com>
2024-01-28 18:23:43 +0000

    model

--- wallet_api_app/models.py
+++ wallet_api_app/models.py
@@ -2,19 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-class WalletUser(models.Model):
-    user_id = models.CharField(max_length=200, null=False, blank=False)
-    wallet_balance = models.FloatField(null=False, blank=False, default=0.0)
-    choices = (
-        ("Active", "Active"),
-        ("Inactive", "Inactive")
-    )
-    status = models.CharField(max_length=200, null=False, blank=False, choices=choices, default="Active")
-    date_created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.user_id
-
 
 class MTNTransaction(models.Model):
     user_id = models.CharField(max_length=200, null=False, blank=False)
